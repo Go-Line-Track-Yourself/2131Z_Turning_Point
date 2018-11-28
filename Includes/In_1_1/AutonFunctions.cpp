4991c5d9f5bb1dac42bb66d4daf560b7e9403a26
Beef-Stu <legocreator75@gmail.com>
2018-11-27 20:32:17 -0700

    l

--- Includes/In_1_1/AutonFunctions.cpp
+++ Includes/In_1_1/AutonFunctions.cpp
@@ -1,3 +1,34 @@
-void AutonDrive(){
-    
+void AutonDrive(double Distance,int Pct=100, int FinalWait=250, int Correction=2){
+    //Local Variables
+    double WheelSize=4*3.1415926535;
+    double Vector=sgn(Distance);
+    double Revolutions= std;;abs(Distance)/WheelSize;
+    int LeftPowSend=0;
+    int RightPowSend=0;
+    //Clear it
+    BLeftMotor.resetRotation();
+    BRightMotor.resetRotation();
+    //Moving to position
+    while(std::abs(BRightMotor.rotation(vex::rotationUnits::rev))<std::abs(Revolutions)){
+        double BLeftMotorValue=BLeftMotor.rotation(vex::rotationUnits::deg);
+        double BRightMotorValue=BRightMotor.rotation(vex::rotationUnits::deg);
+        //Auto Straightening
+        if(std::abs(BLeftMotorValue)>std::abs(BRightMotorValue)){
+            LeftPowSend=Pct-Correction;
+            RightPowSend=Pct;
+        }
+        else if(std::abs(BLeftMotorValue)<std::abs(BRightMotorValue)){
+            LeftPowSend=Pct;
+            RightPowSend=Pct-Correction;
+        }
+        else if(std::abs(BLeftMotorValue)==std::abs(BRightMotorValue)){
+            LeftPowSend=Pct;
+            RightPowSend=Pct;
+        }
+        //Using Auto Straightening to Drive
+        LeftPowSend=LeftPowSend*Vector
+        RightPowSend=RightPowSend*Vector
+        //Send it to Driving Power
+        
+    }
 }

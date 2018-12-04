f9c09814acf00eddc1a7198a66fad86fd07c1c7c
Beef-Stu <legocreator75@gmail.com>
2018-12-03 20:38:21 -0700

    z

--- Includes/In_1_1/AutonFunctions.cpp
+++ Includes/In_1_1/AutonFunctions.cpp
@@ -2,7 +2,7 @@ void AutonMove(double Distance,int Pct=100, int FinalWait=250, int Correction=2)
     //Local Variables
     double WheelSize=4*3.1415926535;
     double Vector=sgn(Distance);
-    double Revolutions= abs(Distance)/WheelSize;
+    double Revolutions=std::abs(Distance)/WheelSize;
     int LPowSend=0;
     int RPowSend=0;
 
@@ -17,20 +17,20 @@ void AutonMove(double Distance,int Pct=100, int FinalWait=250, int Correction=2)
     BLMotor.resetRotation();
     BRMotor.resetRotation();
     //Moving to position
-    while(abs(BRMotor.rotation(vex::rotationUnits::rev))<abs(Revolutions)){
+    while(std::abs(BRMotor.rotation(vex::rotationUnits::rev))<std::abs(Revolutions)){
         double BLMotorValue=BLMotor.rotation(vex::rotationUnits::deg);
         double BRMotorValue=BRMotor.rotation(vex::rotationUnits::deg);
         
         //Auto Straightening
-        if(abs(BLMotorValue)>abs(BRMotorValue)){
+        if(std::abs(BLMotorValue)>std::abs(BRMotorValue)){
             LPowSend=Pct-Correction;
             RPowSend=Pct;
         }
-        else if(abs(BLMotorValue)<abs(BRMotorValue)){
+        else if(std::abs(BLMotorValue)<std::abs(BRMotorValue)){
             LPowSend=Pct;
             RPowSend=Pct-Correction;
         }
-        else if(abs(BLMotorValue)==abs(BRMotorValue)){
+        else if(std::abs(BLMotorValue)==std::abs(BRMotorValue)){
             LPowSend=Pct;
             RPowSend=Pct;
         }
@@ -50,7 +50,7 @@ void AutonMove(double Distance,int Pct=100, int FinalWait=250, int Correction=2)
 
 void AutonTurn(double deg, int LPowSend=50, int RPowSend=50, int FinalWait=25){
     int Direction=sgn(deg);
-    deg=abs(deg);
+    deg=std::abs(deg);
     LPowSend=LPowSend*Direction;
     RPowSend=RPowSend*Direction;
 
@@ -58,7 +58,7 @@ void AutonTurn(double deg, int LPowSend=50, int RPowSend=50, int FinalWait=25){
     Brain.Screen.print(deg);
     Brain.Screen.newLine();
 
-    while(abs(BRMotor.rotation(vex::rotationUnits::rev))<deg){
+    while(std::abs(BRMotor.rotation(vex::rotationUnits::rev))<deg){
        SDMP(LPowSend,-RPowSend);
        vex::task::sleep(1); 
     }
--- Includes/In_1_1/Vars.cpp
+++ Includes/In_1_1/Vars.cpp
@@ -2,10 +2,6 @@ int IntakeIn = 100;
 int IntakeOut = -100;
 bool InvertDrive = false;
 bool WasPressedDrive = false;
-double abs(double Var) {
-	if (Var < 0)  Var = Var * (-1);
-	return Var;
-}
 double sgn(double Var) {
 	if (Var > 0)   Var = 1;
 	else        Var = -1;

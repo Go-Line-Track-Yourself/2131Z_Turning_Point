d2fc1350bb9d23765e60de6a6b9e5cd29e557f34
Beef-Stu <legocreator75@gmail.com>
2018-12-05 23:50:05 -0700

    z

--- Includes/In_1_1/Auton.cpp
+++ Includes/In_1_1/Auton.cpp
@@ -5,29 +5,30 @@ void Auton(){
     //AutonIntk(ON or !ON, In or !In);
     //AutonFlyC(Go or !Go);
     //AutonFlip();
+    //AutonIndx(Go or !Go);
 }
 void FrontB(){
-    AutonFlyC(Go);
-    AutonIntk(ON);
-    AutonMove(46);
-    AutonTurn(-45);
-    AutonIntk(!ON);
-    AutonMove(-19);
-    AutonFlip();
-    AutonTurn(45);
-    AutonMove(-28);
-    AutonTurn(90);
-    AutonMove(10);
-    AutonMove(-);
-    AutonIndx(Go);
-    AutonMove(-);
-    AutonIntk(ON);
-    AutonMove(-);
-    AutonIndx(!Go);
-    AutonFlyC(!Go);
-    AutonIntk(!ON);
-    AutonTurn(90);
-    AutonMove(-38);
+    AutonFlyC(Go);  //Turn on Flywheel
+    AutonIntk(ON);  //Turn on Intake
+    AutonMove(46);  //Go forward 46 Inches
+    AutonTurn(-45); //Turn left 45 degrees
+    AutonIntk(!ON); //Turn off Intake
+    AutonMove(-19); //Go backwards 19 Inches
+    AutonFlip();    //Flip Cap
+    AutonTurn(45);  //Turn right 45 degrees
+    AutonMove(-28); //Go backwards 28 Inches
+    AutonTurn(90);  //Turn right 90 degrees
+    AutonMove(10);  //Go forwards 10 Inches
+    AutonMove(-);   //Go back to Middle Flag Shot Spot
+    AutonIndx(Go);  //Turn on Indexer
+    AutonMove(-);   //Go back to Top Flad Shot Spot
+    AutonIntk(ON);  //Turn on Intake to Move Ball into Indexer and Shoot it
+    AutonMove(-);   //Go backwards to be Parallel with Platform
+    AutonIndx(!Go); //Turn off Indexer
+    AutonFlyC(!Go); //Turn off Flywheel
+    AutonIntk(!ON); //Turn off Intake
+    AutonTurn(90);  //Turn right 90
+    AutonMove(-38); //Go backwards 38 Inches
 }
 /*void BackB{
 
@@ -36,25 +37,25 @@ void BackR{
 
 }*/
 void FrntR{
-    AutonFlyC(Go);
-    AutonIntk(ON);
-    AutonMove(46);
-    AutonTurn(45);
-    AutonIntk(!ON);
-    AutonMove(-19);
-    AutonFlip();
-    AutonTurn(-45);
-    AutonMove(-28);
-    AutonTurn(-90);
-    AutonMove(10);
-    AutonMove(-);
-    AutonIndx(Go);
-    AutonMove(-);
-    AutonIntk(ON);
-    AutonMove(-);
-    AutonIndx(!Go);
-    AutonFlyC(!Go);
-    AutonIntk(!ON);
-    AutonTurn(-90);
-    AutonMove(-38);
+    AutonFlyC(Go);  //Turn on Flywheel
+    AutonIntk(ON);  //Turn on Intake
+    AutonMove(46);  //Go forward 46 Inches
+    AutonTurn(45);  //Turn right 45 degrees
+    AutonIntk(!ON); //Turn off Intake
+    AutonMove(-19); //Go backwards 19 Inches
+    AutonFlip();    //Flip Cap
+    AutonTurn(-45); //Turn left 45 degrees
+    AutonMove(-28); //Go backwards 28 Inches
+    AutonTurn(-90); //Turn left 90 degrees
+    AutonMove(10);  //Go forwards 10 Inches
+    AutonMove(-);   //Go back to Middle Flag Shot Spot
+    AutonIndx(Go);  //Turn on Indexer
+    AutonMove(-);   //Go back to Top Flad Shot Spot
+    AutonIntk(ON);  //Turn on Intake to Move Ball into Indexer and Shoot it
+    AutonMove(-);   //Go backwards to be Parallel with Platform
+    AutonIndx(!Go); //Turn off Indexer
+    AutonFlyC(!Go); //Turn off Flywheel
+    AutonIntk(!ON); //Turn off Intake
+    AutonTurn(-90); //Turn left 90
+    AutonMove(-38); //Go backwards 38 Inches
 }
